bc25c563658ef24f7cfc5e566997cf179cbc2f78
gsharan98 <gsharan9898@gmail.com>
2024-10-16 14:51:05 +0400

    coodingscool.py
    cooding is cool

---
+++ Exercise 01/cooding is cool.py
@@ -0,0 +1,10 @@
+#setting the first word to the variable word1
+word1 = "coding"
+
+#setting the second word to the variable word2
+word2 = "is"
+
+#setting the third word to the variable word3
+word3 = "cool"
+c = word1 + " " + word2 + " " + word3
+print(c)
