5853336e69d3c6e3c65430debbc6b029884d1038
gsharan98 <gsharan9898@gmail.com>
2024-10-27 12:36:38 +0400

    primitive quiz.py

---
+++ Exercise 04/primitive quiz basic.py
@@ -0,0 +1,10 @@
+### Steps:
+#1. Write a program that asks the user "What is the capital of France?" and waits for their response.
+#2. If the user's answer is correct (i.e., "Paris"), the program should print a message saying the answer is correct.
+#3. If the answer is incorrect, the program should print a message saying the answer is wrong.
+ 
+answer= input("what is the capital of france:")
+if answer ==  "paris":                 #assigning the answer to given question.
+   print ("answer is correct") 
+else:                    
+   print("the answer is incorrect")    # assigning to print ("the answer is incorrecct") if answer is not paris.
