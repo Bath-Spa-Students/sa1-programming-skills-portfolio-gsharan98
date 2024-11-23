d86b63539e0943d513b3f4c693ffd4e2cbbd6b15
gsharan98 <gsharan9898@gmail.com>
2024-11-23 09:16:58 +0400

    .py

--- Exercise 06/Brute force attack.py
+++ Exercise 06/Brute force attack.py
@@ -1,42 +1,24 @@
-
-#optional requirementz
-#Modify the program to include a maximum of 5 password attempts. If the user enters the wrong password, inform them of the remaining attempts. If the maximum number of attempts is reached, inform the user that the authorities have been alerted.
 #Write a program that simulates a password entry system. The correct password is defined as 12345. The program should keep asking the user to enter the password until they provide the correct one.
 
+### Basic Requirements:
+#1. Define the correct password.
+#2. Use a while loop to repeatedly ask the user for the password until the correct one is entered.
+#3. Output an appropriate message when the correct password is entered.
 
-#assgning a correct passcode,defining correct password
-valid_passcode = "12345"
 
-# amount of attempts done by user
-max_num_of_attempts = 5
-
-# keeping track of how much attempts has been made from starting
-attempts = 0
-
-# this code is used to loop until the password input by the user is correct
-while attempts < max_num_of_attempts:
-    # asking user to input the passcode
-    user_passcode = input(" enter the  correct passcode: ").strip() 
+def password_for_user_verification_system():        # asksing the user for accurate passcode and then checking if the passcode is correct
+    accurate_passcode = "12345"                     # The accurate passcode for user verification
     
-    # Masking  the entire password with asterisks for privacy reasons
-    hidden_password = '*' * len(user_passcode)
-    
-    # after every one incorrect answer increase the attempt
-    attempts += 1
+    while True:#this code is used to loop until the password input by the user is correct
+        user_password = input("input the accurate passcode: ")#assigning a question to a variable for the user to input a password
+        
+        if user_password == accurate_passcode:
+            print("the passcode is correct.!")            #it will print this output if the passcode is correct
+            break#code that breaks the loop if answer given is correct 
+        else:
+            print("opps!incorrect answer,try again.")     #it will print this output , if the passcode is incorrect.
 
-    # Checking if the inputed password matches the  user_passcode
-    if user_passcode == valid_passcode:
-        # If the answer is correct ,it will end loop instantly
-        print("passcode is correct ,welcome")
-        break
-    else:
-        # if thae answer is incorrect it will print wrong passcode
-        print(f"wrong passcode: {hidden_password}")
-        
-        # giving hints if user is on their second to last attempts
-        if attempts == max_num_of_attempts - 1:
-            print("Hint: The password is an interger.")
-        
-        # after finishing of all the attempts they will be blocked
-        if attempts == max_num_of_attempts:
-            print("you have been blocked for a while.")
+password_for_user_verification_system()
+
+
+
